301a0731f0f6d2a82ab17694c3d6777a63c761d4
FabianAMoreno <fabian.moreno@syntax-institut.de>
2024-09-06 12:45:41 +0200

    Initial Commit

---
+++ Music Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music Player
+//
+//  Created by Fabian Moreno on 06.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by Fabian Moreno on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
